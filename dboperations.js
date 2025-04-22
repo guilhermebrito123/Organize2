f412145ecf39690d0c9d7b68458e85ed90685687
guilhermebrito123 <gbfsilva@sga.pucminas.br>
2025-04-22 15:53:26 -0300

    Subindo resto das operações definidas em dboperations

--- dboperations.js
+++ dboperations.js
@@ -6,7 +6,7 @@ const sql = require("mssql");
 async function getUsuarios() {
   try {
     let pool = await sql.connect(config);
-    let organize = await pool.request().query("select * from Usuarios");
+    let organize = await pool.request().query("select * from [dbo].[Usuarios]");
     return organize.recordsets;
   } catch (error) {
     console.error("Erro ao buscar usuários:", error);
@@ -14,4 +14,79 @@ async function getUsuarios() {
   }
 }
 
-module.exports = { getUsuarios: getUsuarios };
+async function updateUsuario(usuario) {
+  try {
+    let pool = await sql.connect(config);
+    let usuario = await pool
+      .request()
+      .input("input_parameter", sql.Int, usuario.Id)
+      .query(`Update [dbo].[Usuarios]
+    SET 
+    [Nome] = '${usuario.Nome}',
+    [Email] = '${usuario.Email}',
+    [Senha] = '${usuario.Senha}'
+    WHERE ID = @input_parameter
+    `);
+    return usuario.recordsets;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function getUsuario(usuarioId) {
+  try {
+    let pool = await sql.connect(config);
+    let organize = await pool
+      .request()
+      .input("input_parameter", sql.Int, usuario.Id)
+      .query("select * from [dbo].[Usuarios] WHERE ID = @input_parameter");
+    return organize.recordsets;
+  } catch (error) {
+    console.error("Erro ao buscar usuários:", error);
+    throw error;
+  }
+}
+
+async function delUsuario(usuarioId) {
+  try {
+    let pool = await sql.connect(config);
+    let organize = await pool
+      .request()
+      .input("input_parameter", sql.Int, usuario.Id)
+      .query("DELETE from [dbo].[Usuarios] WHERE ID = @input_parameter");
+    return organize.recordsets;
+  } catch (error) {
+    console.error("Erro ao buscar usuários:", error);
+    throw error;
+  }
+}
+
+async function addUsuario(usuario) {
+  try {
+    let pool = await sql.connect(config);
+    let organize = await pool.request().query(`INSERT INTO [dbo].[Usuarios]
+    (
+    [Id],
+    [Nome],
+    [Email],
+    [Senha]
+    )VALUES(
+      '${usuario.Id}',
+      '${usuario.Nome}',
+      '${usuario.Email}',
+      '${usuario.Senha}'
+    )`);
+    return organize.recordsets;
+  } catch (error) {
+    console.error("Erro ao buscar usuários:", error);
+    throw error;
+  }
+}
+
+module.exports = {
+  getUsuarios: getUsuarios,
+  updateUsuario: updateUsuario,
+  getUsuario: getUsuario,
+  delUsuario: delUsuario,
+  addUsuario: addUsuario
+};
--- src/app/api.js
+++ src/app/api.js
@@ -18,11 +18,40 @@ router.use((request, response, next) => {
   next();
 });
 
-router.route("/Usuarios").get((request, Response) => {
+router.route("/Usuarios").get((request, response) => {
   dboperations.getUsuarios().then(result => {
-    Response.json(result[0]);
+    response.json(result[0]);
   });
 });
+
+router.route('Usuarios').patch((request,response) => {
+  let usuario = {...request.body}
+
+  dboperations.updateUsuario(usuario).then(result => 
+    response.status(204).json(result)
+  )
+})
+
+router.route('Usuarios/:id').get((request, response) => {
+  dboperations.getUsuario(request.params.id),then(result => {
+    response.json(result[0])
+  })
+})
+
+router.route('Usuarios/:id').delete((request, response) => {
+  dboperations.delUsuario(request.params.id),then(result => {
+    response.json(result[0])
+  })
+})
+
+router.route('Usuarios').post((request,response) => {
+  let usuario = {...request.body}
+
+  dboperations.addUsuario(usuario).then(result => {
+    response.status(201).json(result)
+  })
+})
+
 
 var port = process.env.PORT || 8090;
 app.listen(port);
